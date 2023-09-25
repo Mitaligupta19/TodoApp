b2ea2d450a4a9e715a99962f4b0c8364f74d47da
Mitaligupta19 <141215898+Mitaligupta19@users.noreply.github.com>
2023-09-25 16:53:09 +0530

    Initial Commit

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,13 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Mitali Gupta on 25/09/23.
+//
+
+import Foundation
+
+class Item: Codable {
+    var title: String = ""
+    var done: Bool = false
+}
